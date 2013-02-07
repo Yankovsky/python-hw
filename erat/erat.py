2abb7001fdf6e2b9c905196b40e0efd06b8557de
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-02-07 21:24:54 +0400

    Erat improved

--- erat/erat.py
+++ erat/erat.py
@@ -7,4 +7,4 @@ def eratosthenes_sieve(upto):
     Fully functional implementation of eratosthenes sieve
     Upto - exclusive upper limit. So eratosthenes_sieve(7) = [2,3,5]
     """
-    return sorted(reduce(sub, [set(range(y * y, upto, 2 * y)) for y in [x for x in range(3, int(ceil(upto ** 0.5)), 2)]], set(chain([2], set(range(3, upto, 2))))))
+    return sorted(reduce(sub, [set(range(x * x, upto, 2 * x)) for x in range(3, int(ceil(upto ** 0.5)), 2)], set(chain([2], set(range(3, upto, 2))))))
